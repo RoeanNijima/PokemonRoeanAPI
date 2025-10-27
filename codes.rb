90ccc69424361adf0c4ae865497fa1a62fa3a217
RoeanNijima <144230311+RoeanNijima@users.noreply.github.com>
2025-10-27 19:47:08 +0000

    Update codes.rb

--- codes.rb
+++ codes.rb
@@ -2,5 +2,9 @@
   "testcode1" => {
     "pokemon" => ["PIKACHU,5,1"],
     "items" => ["POKEBALL,10", "POTION,5"]
+  },
+  "testcode1" => {
+    "pokemon" => ["MAGNEMITE,25,0"],
+    "items" => ["MASTERBALL,1"]
   }
 }
