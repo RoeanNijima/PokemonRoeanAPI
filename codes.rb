11b0c27961aa5c4cb4c4473b2073d8994a4250b2
RoeanNijima <144230311+RoeanNijima@users.noreply.github.com>
2025-10-28 19:14:57 +0000

    Update codes.rb

--- codes.rb
+++ codes.rb
@@ -2,6 +2,9 @@
   "starter" => {
     "items" => ["POKEBALL,10", "FRESHWATER,5","REVIVE,1"]
   },
+  "hunter" => {
+    "items" => ["TM54,1", "TM73,1"]
+  },
   "spooky" => {
     "pokemon" => ["GASTLY,5,1,Spooky"]
   }
